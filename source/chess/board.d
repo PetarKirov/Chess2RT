d05ff192ff6ab7971e999245e4bed768ae8c9ed5
glavchev <glavchev_1991@abv.bg>
2016-08-28 16:31:33 +0300

    Implemented all pieces, added unittests

--- source/chess/board.d
+++ source/chess/board.d
@@ -6,43 +6,52 @@ import std.algorithm, std.array, std.container;
 
 struct BoardPosition
 {
-	uint x;
-	uint y;
-	this(uint x, uint y)
+	int x;
+	int y;
+	this(int x, int y)
 	{
 		this.x = x;
 		this.y = y;
 	}
 	bool isValid()
 	{
-		return x < 8 && y < 8;
+		return x < 8 && x > -1 && y < 8 && y > -1;
 	}
 }
 
 struct LastMove
 {
 	BoardPosition pos1;
-	Piece* pieceAtPos1;
+	Piece pieceAtPos1;
 	BoardPosition pos2;
-	Piece* pieceAtPos2;
+	Piece pieceAtPos2;
 }
 
 struct PlayerSettings
 {
-	BoardPosition kingPosition;
+	BoardPosition kingPosition = BoardPosition(-1,-1);
 }
 
 class Board
 {
     private
     {
-        Piece*[8][8] board;
+        Piece[8][8] board;
 		PieceColor activePlayer = PieceColor.White;
 		PlayerSettings[2] playersSettings;
 		LastMove lastMove;
     }
 
 	this()
+	{
+		reset();
+	}
+	this(char[64] asciiRepresentation)
+    {
+		reset(asciiRepresentation);
+    }
+
+	void reset()
 	{
 		char[64] s = "rnbqkbnr" ~
 			"pppppppp" ~
@@ -52,20 +61,21 @@ class Board
 			"........" ~
 			"PPPPPPPP" ~
 			"RNBQKBNR";
-		this(s);
+		reset(s);
 	}
-	this(char[64] asciiRepresentation)
-    {
-		//foreach(i, c; asciiRepresentation)
-		//    this[i/8,i%8] = generatePiece(c, &this);
-    }
 
-	ref Piece* opIndex(BoardPosition pos)
+	void reset(char[64] asciiRepresentation)
+	{
+		foreach(i, c; asciiRepresentation)
+		    this[i/8,i%8] = generatePiece(c, this, BoardPosition(i/8, i%8));
+		updatePieces();
+	}
+	ref Piece opIndex(BoardPosition pos)
 	{
 		return board[pos.x][pos.y];
 	}
 
-    ref Piece* opIndex(uint r, uint c)
+    ref Piece opIndex(uint r, uint c)
     {
         return board[r][c];
     }
@@ -73,14 +83,17 @@ class Board
 	bool isCheck()
 	{
 		BoardPosition activeKing = (activePlayer == PieceColor.White) ? playersSettings[0].kingPosition : playersSettings[1].kingPosition;
-		for(uint i=0; i<8; ++i)
+		if(activeKing.isValid())
 		{
-			for(uint j=0; j<8; ++j)
+			for(uint i=0; i<8; ++i)
 			{
-				Piece* currentPiece = board[i][j];
-				if(currentPiece && currentPiece.canHit(activeKing))
+				for(uint j=0; j<8; ++j)
 				{
-					return true;
+					Piece currentPiece = board[i][j];
+					if(currentPiece && currentPiece.canHit(activeKing))
+					{
+						return true;
+					}
 				}
 			}
 		}
@@ -89,6 +102,7 @@ class Board
 
 	bool isMate()
 	{
+		// TODO
 		return false;
 	}
 
@@ -96,34 +110,82 @@ class Board
 	{
 		return activePlayer;
 	}
-	private:
+	void switchActivePlayer()
+	{
+		activePlayer = (activePlayer == PieceColor.White) ? PieceColor.Black : PieceColor.White;
+	}
+
+private:
 	void simpleMove(BoardPosition pos1, BoardPosition pos2)
 	{
-		Piece* piece1 = this[pos1];
-		Piece* piece2 = this[pos2];
+		Piece piece1 = this[pos1];
+		Piece piece2 = this[pos2];
 
 		this[pos1] = null;
 		this[pos2] = piece1;
 		updateLastMove(pos1, piece1, pos2, piece2);
+		updatePieces();
 	}
-	private:
-	void updateLastMove(BoardPosition pos1, Piece* pieceAtPos1, 
-						BoardPosition pos2, Piece* pieceAtPos2)
+
+	void updateLastMove(BoardPosition pos1, Piece pieceAtPos1, 
+						BoardPosition pos2, Piece pieceAtPos2)
 	{
 		this.lastMove.pos1 = pos1;
 		this.lastMove.pieceAtPos1 = pieceAtPos1;
 		this.lastMove.pos2 = pos2;
 		this.lastMove.pieceAtPos2 = pieceAtPos2;
 	}
-	private:
+
 	void undoLastMove()
 	{
-		this[lastMove.pos1] = lastMove.pieceAtPos2;
-		this[lastMove.pos2] = lastMove.pieceAtPos1;
+		this[lastMove.pos1] = lastMove.pieceAtPos1;
+		this[lastMove.pos2] = lastMove.pieceAtPos2;
+		updatePieces();
 	}
+
+	void updatePieces()
+	{
+		PlayerSettings* whiteSettings = &playersSettings[0];
+		PlayerSettings* blackSettings = &playersSettings[1];
+		for(uint i=0; i<8; ++i)
+		{
+			for(uint j=0; j<8; ++j)
+			{
+				Piece currentPiece = board[i][j];
+				if(currentPiece)
+				{
+					currentPiece.position = BoardPosition(i,j);
+					if(currentPiece.getType() == PieceEnum.King)
+					{
+						if(currentPiece.color == PieceColor.White)
+						{
+							whiteSettings.kingPosition = currentPiece.position;
+						}
+						else
+						{
+							blackSettings.kingPosition = currentPiece.position;
+						}
+					}
+				}
+			}
+		}
+
+		for(uint i=0; i<8; ++i)
+		{
+			for(uint j=0; j<8; ++j)
+			{
+				Piece currentPiece = board[i][j];
+				if(currentPiece)
+				{
+					currentPiece.updatePositionsToHit();
+				}
+			}
+		}
+	}
+public:
 	bool move(BoardPosition pos1, BoardPosition pos2)
 	{
-		if(!this[pos1] || !this[pos1].canHit(pos2))
+		if(this[pos1] is null || !this[pos1].canHit(pos2))
 		{
 			return false;
 		}
@@ -136,137 +198,160 @@ class Board
 			undoLastMove();
 			return false;
 		}
-
-		//update all figures
 		return true;
 	}
+	unittest
+	{
+		Board b = new Board("........" ~
+							"p......." ~
+							"..p.p..." ~
+							"....P..." ~
+							".....p.." ~
+							"....P.P." ~
+							"........" ~
+							"........");
+		assert(b.move(BoardPosition(1,0), BoardPosition(3,0)));
+		assert(b[3,0] && b[3,0].getType() == PieceEnum.Pawn && b[3,0].position == BoardPosition(3,0));
 
-    void toString(scope void delegate(const(char)[]) sink) const
-    {
-    }
+		assert(!b.move(BoardPosition(2,4), BoardPosition(3,4))); //illegal move
+
+		assert(b.move(BoardPosition(4,5), BoardPosition(5,6))); // take piece with pawn
+		assert(!b[4,5]);
+		assert(b[5,6].color == PieceColor.Black && b[5,6].getType() == PieceEnum.Pawn);
+
+		//test with check
+		b = new Board("........" ~
+					  "p..k...." ~
+					  "..p.p..." ~
+					  "....P..." ~
+					  "B....p.." ~
+					  "....P.P." ~
+					  "........" ~
+					  "........");
+		b.activePlayer = PieceColor.Black;
+		assert(!b.move(BoardPosition(2,2), BoardPosition(3,2))); //illegal move, because it would leave black king in check
+		// check if the illegal move changed anything on the board (shouldnt happen)
+		assert(b[2,2] && b[2,2].getType() == PieceEnum.Pawn && b[2,2].position == BoardPosition(2,2)); 
+		assert(b.move(BoardPosition(1,0), BoardPosition(2,0))); //legal move, king is protected
+	}
+
 	/************************************************************/
 	//return value indicates whether to continue or not
-	private
+	bool appendPosition(ref BoardPosition[] arr, BoardPosition pos, PieceColor color)
 	{
-		bool appendPosition(BoardPosition[] arr, BoardPosition pos, PieceColor color)
-		{
-			Piece* current = this[pos];
-			if(current == null)
-			{
-				arr ~= pos;
-				return true;
-			}
-			if(current.color != color)
-				arr ~= pos;
+		if(pos.x > 7 || pos.x < 0 || pos.y > 7 || pos.y < 0)
 			return false;
-		}
-		BoardPosition[] getLeftHorizontal(PieceColor color, BoardPosition pos)
+		Piece current = this[pos];
+		if(current is null)
 		{
-			BoardPosition[] result;
-			result.reserve(7);
-			for(int x = pos.x - 1; x >= 0; --x)
-			{
-				if(appendPosition(result, BoardPosition(x, pos.y), color))
-					continue;
-				break;
-			}
-			return result;
+			arr ~= pos;
+			return true;
 		}
-
-		BoardPosition[] getRightHorizontal(PieceColor color, BoardPosition pos)
-		{
-			BoardPosition[] result;
-			result.reserve(7);
-			for(int x = pos.x + 1; x < 8; ++x)
-			{
-				if(appendPosition(result, BoardPosition(x, pos.y), color))
-					continue;
-				break;
-			}
-			return result;
-		}
-
-		BoardPosition[] getUpVertical(PieceColor color, BoardPosition pos)
-		{
-			BoardPosition[] result;
-			result.reserve(7);
-			for(int y = pos.y - 1; y >= 0; --y)
-			{
-				if(appendPosition(result, BoardPosition(pos.x, y), color))
-					continue;
-				break;
-			}
-			return result;
-		}
-
-		BoardPosition[] getDownVertical(PieceColor color, BoardPosition pos)
-		{
-			BoardPosition[] result;
-			result.reserve(7);
-			for(int y = pos.y + 1; y < 8; ++y)
-			{
-				if(appendPosition(result, BoardPosition(pos.x, y), color))
-					continue;
-				break;
-			}
-			return result;
-		}
-
-		BoardPosition[] getUpRightDiagonal(PieceColor color, BoardPosition pos)
-		{
-			BoardPosition[] result;
-			result.reserve(7);
-			for(int x = pos.x - 1, y = pos.y + 1; x >=0 && y < 8 ; --x, ++y)
-			{
-				if(appendPosition(result, BoardPosition(x, y), color))
-					continue;
-				break;
-			}
-			return result;
-		}
-
-		BoardPosition[] getUpLeftDiagonal(PieceColor color, BoardPosition pos)
-		{
-			BoardPosition[] result;
-			result.reserve(7);
-			for(int x = pos.x - 1, y = pos.y - 1; x >=0 && y >= 0 ; --x, --y)
-			{
-				if(appendPosition(result, BoardPosition(x, y), color))
-					continue;
-				break;
-			}
-			return result;
-		}
-
-		BoardPosition[] getDownRightDiagonal(PieceColor color, BoardPosition pos)
-		{
-			BoardPosition[] result;
-			result.reserve(7);
-			for(int x = pos.x + 1, y = pos.y - 1; x < 8 && y >= 0; ++x, --y)
-			{
-				if(appendPosition(result, BoardPosition(x, y), color))
-					continue;
-				break;
-			}
-			return result;
-		}
-
-		BoardPosition[] getDownLeftDiagonal(PieceColor color, BoardPosition pos)
-		{
-			BoardPosition[] result;
-			result.reserve(7);
-			for(int x = pos.x + 1, y = pos.y + 1; x < 8 && y < 8 ; ++x, ++y)
-			{
-				if(appendPosition(result, BoardPosition(x, y), color))
-					continue;
-				break;
-			}
-			return result;
-		}
+		if(current.color != color)
+			arr ~= pos;
+		return false;
 	}
-}
+	BoardPosition[] getUpVertical(PieceColor color, BoardPosition pos)
+	{
+		BoardPosition[] result;
+		result.reserve(7);
+		for(int x = pos.x - 1; ; --x)
+		{
+			if(appendPosition(result, BoardPosition(x, pos.y), color))
+				continue;
+			break;
+		}
+		return result;
+	}
 
-void test()
-{
-    //Board b = Board();
+	BoardPosition[] getDownVertical(PieceColor color, BoardPosition pos)
+	{
+		BoardPosition[] result;
+		result.reserve(7);
+		for(int x = pos.x + 1; ; ++x)
+		{
+			if(appendPosition(result, BoardPosition(x, pos.y), color))
+				continue;
+			break;
+		}
+		return result;
+	}
+
+	BoardPosition[] getLeftHorizontal(PieceColor color, BoardPosition pos)
+	{
+		BoardPosition[] result;
+		result.reserve(7);
+		for(int y = pos.y - 1; ; --y)
+		{
+			if(appendPosition(result, BoardPosition(pos.x, y), color))
+				continue;
+			break;
+		}
+		return result;
+	}
+
+	BoardPosition[] getRightHorizontal(PieceColor color, BoardPosition pos)
+	{
+		BoardPosition[] result;
+		result.reserve(7);
+		for(int y = pos.y + 1; ; ++y)
+		{
+			if(appendPosition(result, BoardPosition(pos.x, y), color))
+				continue;
+			break;
+		}
+		return result;
+	}
+
+	BoardPosition[] getUpRightDiagonal(PieceColor color, BoardPosition pos)
+	{
+		BoardPosition[] result;
+		result.reserve(7);
+		for(int x = pos.x - 1, y = pos.y + 1; ; --x, ++y)
+		{
+			if(appendPosition(result, BoardPosition(x, y), color))
+				continue;
+			break;
+		}
+		return result;
+	}
+
+	BoardPosition[] getUpLeftDiagonal(PieceColor color, BoardPosition pos)
+	{
+		BoardPosition[] result;
+		result.reserve(7);
+		for(int x = pos.x - 1, y = pos.y - 1; ; --x, --y)
+		{
+			if(appendPosition(result, BoardPosition(x, y), color))
+				continue;
+			break;
+		}
+		return result;
+	}
+
+	BoardPosition[] getDownRightDiagonal(PieceColor color, BoardPosition pos)
+	{
+		BoardPosition[] result;
+		result.reserve(7);
+		for(int x = pos.x + 1, y = pos.y - 1; ; ++x, --y)
+		{
+			if(appendPosition(result, BoardPosition(x, y), color))
+				continue;
+			break;
+		}
+		return result;
+	}
+
+	BoardPosition[] getDownLeftDiagonal(PieceColor color, BoardPosition pos)
+	{
+		BoardPosition[] result;
+		result.reserve(7);
+		for(int x = pos.x + 1, y = pos.y + 1; ; ++x, ++y)
+		{
+			if(appendPosition(result, BoardPosition(x, y), color))
+				continue;
+			break;
+		}
+		return result;
+	}
 }
--- source/chess/piece.d
+++ source/chess/piece.d
@@ -9,43 +9,78 @@ enum PieceColor
     Black = 1
 }
 
-//enum ArmyType
-//{
-//    Classic =   0,
-//    Nemesis =   1,
-//    Empowered = 2,
-//    Reaper =    3,
-//    TwoKings =  4,
-//    Animals =   5,
-//
-//    ArmyTypeMask = 7
-//}
-
-Piece* generatePiece(char c, Board* board)
+enum PieceEnum : byte
 {
-	return null;
+	Pawn = 1,
+	Knight = 2,
+	Bishop = 3,
+	Rook = 4,
+	Queen = 5,
+	King = 6
+}
+
+Piece generatePiece(char c, Board board, BoardPosition pos)
+{
+	switch(c)
+	{
+		case 'K':
+			return new King(PieceColor.White, pos, board);
+		case 'R':
+			return new Rook(PieceColor.White, pos, board);
+		case 'N':
+			return new Knight(PieceColor.White, pos, board);
+		case 'B':
+			return new Bishop(PieceColor.White, pos, board);
+		case 'Q':
+			return new Queen(PieceColor.White, pos, board);
+		case 'P':
+			return new Pawn(PieceColor.White, pos, board);
+		case 'k':
+			return new King(PieceColor.Black, pos, board);
+		case 'r':
+			return new Rook(PieceColor.Black, pos, board);
+		case 'n':
+			return new Knight(PieceColor.Black, pos, board);
+		case 'b':
+			return new Bishop(PieceColor.Black, pos, board);
+		case 'q':
+			return new Queen(PieceColor.Black, pos, board);
+		case 'p':
+			return new Pawn(PieceColor.Black, pos, board);
+		default:
+			return null;
+	}
 }
 
 abstract class Piece
 {
-
 	PieceColor color;
 	BoardPosition position;
-	Board* parentBoard;
+	Board parentBoard;
 	BoardPosition[] positionsToHit;
 
-	this(PieceColor color, BoardPosition initialPosition, Board* parentBoard)
+	this(PieceColor color, BoardPosition initialPosition, Board parentBoard)
 	{
 		this.color = color;
+		this.position = initialPosition;
+		this.parentBoard = parentBoard;
 	}
 	
-	abstract BoardPosition[] getPositionsToHitImpl();
+	bool move(BoardPosition target)
+	{
+		return parentBoard.move(position, target);
+	}
+	abstract PieceEnum getType();
 
 	void updatePositionsToHit()
 	{
 		positionsToHit = getPositionsToHitImpl();
 	}
 
+	BoardPosition[] getPositionsToHit()
+	{
+		return positionsToHit;
+	}
 	bool canHit(BoardPosition pos)
 	{
 		foreach (position ; positionsToHit)
@@ -57,20 +92,20 @@ abstract class Piece
 		}
 		return false;
 	}
-
-    void toString(scope void delegate(const(char)[]) sink) const
-    {
-        return;
-    }
+	abstract BoardPosition[] getPositionsToHitImpl();
 }
 
 class Pawn : Piece
 {
-	this(PieceColor color, BoardPosition initialPosition, Board* parentBoard)
+	this(PieceColor color, BoardPosition initialPosition, Board parentBoard)
 	{
 		super(color, initialPosition, parentBoard);
 	}
 
+	override PieceEnum getType()
+	{
+		return PieceEnum.Pawn;
+	}
 	override BoardPosition[] getPositionsToHitImpl()
 	{
 		BoardPosition[] result;
@@ -78,9 +113,9 @@ class Pawn : Piece
 		if(color == PieceColor.White && position.x > 0)
 		{
 			BoardPosition front = BoardPosition(position.x - 1, position.y);
-			bool emptyFront = ((*parentBoard)[front] == null);
-			Piece * leftFront = (position.y > 0) ? (*parentBoard)[position.x-1, position.y-1] : null;
-			Piece * rightFront = (position.y < 7) ? (*parentBoard)[position.x-1, position.y+1] : null;
+			bool emptyFront = (parentBoard[front] is null);
+			Piece leftFront = (position.y > 0) ? parentBoard[position.x-1, position.y-1] : null;
+			Piece rightFront = (position.y < 7) ? parentBoard[position.x-1, position.y+1] : null;
 			if(emptyFront) 
 			{
 				result ~= front;
@@ -95,7 +130,7 @@ class Pawn : Piece
 			}
 			if(position.x == 6 && emptyFront)
 			{
-				if((*parentBoard)[4, position.y] == null)
+				if(parentBoard[4, position.y] is null)
 				{
 					result ~= BoardPosition(4, position.y);
 				}
@@ -104,9 +139,9 @@ class Pawn : Piece
 		else if(color == PieceColor.Black && position.x < 7)
 		{
 			BoardPosition front = BoardPosition(position.x + 1, position.y);
-			bool emptyFront = ((*parentBoard)[front] == null);
-			Piece * leftFront = (position.y > 0) ? (*parentBoard)[position.x+1, position.y-1] : null;
-			Piece * rightFront = (position.y < 7) ? (*parentBoard)[position.x+1, position.y+1] : null;
+			bool emptyFront = (parentBoard[front] is null);
+			Piece leftFront = (position.y > 0) ? parentBoard[position.x+1, position.y-1] : null;
+			Piece rightFront = (position.y < 7) ? parentBoard[position.x+1, position.y+1] : null;
 			if(emptyFront) 
 			{
 				result ~= front;
@@ -121,7 +156,7 @@ class Pawn : Piece
 			}
 			if(position.x == 1 && emptyFront)
 			{
-				if((*parentBoard)[3, position.y] == null)
+				if(parentBoard[3, position.y] is null)
 				{
 					result ~= BoardPosition(3, position.y);
 				}
@@ -130,101 +165,232 @@ class Pawn : Piece
 		return result;
 	}
 }
+unittest
+{
+	Board b = new Board("........" ~
+						"p......." ~
+						"..p.p..." ~
+						"....P..." ~
+						".....p.." ~
+						"....P.P." ~
+						"........" ~
+						"........");
+	Piece p1 = b[1,0];
+	p1.updatePositionsToHit();
+	BoardPosition[] p1PosToHit = p1.getPositionsToHit();
+	assert(p1.getPositionsToHit().length == 2);
+	assert(p1PosToHit[0].x == 2 && p1PosToHit[0].y == 0);
+	assert(p1PosToHit[1].x == 3 && p1PosToHit[1].y == 0);
+	Piece p2 = b[2,2];
+	p2.updatePositionsToHit();
+	BoardPosition[] p2PosToHit = p2.getPositionsToHit();
+	assert(p2.getPositionsToHit().length == 1);
+	assert(p2PosToHit[0].x == 3 && p2PosToHit[0].y == 2);
+
+	Piece p3 = b[4,5];
+	p3.updatePositionsToHit();
+	assert(p3.getPositionsToHit().length == 3);
+
+	Piece p4 = b[5,4];
+	p4.updatePositionsToHit();
+	assert(p4.getPositionsToHit().length == 2);
+
+	Piece p5 = b[2,4];
+	p5.updatePositionsToHit();
+	assert(p5.getPositionsToHit().length == 0);
+}
 
 class Rook : Piece
 {
-	this(PieceColor color, BoardPosition initialPosition, Board* parentBoard)
+	this(PieceColor color, BoardPosition initialPosition, Board parentBoard)
 	{
 		super(color, initialPosition, parentBoard);
+	}
+
+	override PieceEnum getType()
+	{
+		return PieceEnum.Rook;
 	}
 
 	override BoardPosition[] getPositionsToHitImpl()
 	{
 		BoardPosition[] result;
-		
+		result.reserve(20);
+		result ~= parentBoard.getDownVertical(color, position);
+		result ~= parentBoard.getUpVertical(color, position);
+		result ~= parentBoard.getRightHorizontal(color, position);
+		result ~= parentBoard.getLeftHorizontal(color, position);
 		return result;
 	}
 }
-void test()
+unittest
 {
-	//import std.stdio;
-	//
-	//foreach (army; __traits(allMembers, ArmyType))
-	//    foreach (color; __traits(allMembers, PieceColor))
-	//        foreach (type; __traits(allMembers, PieceType))
-	//        {
-	//            auto p = Piece(
-	//                __traits(getMember, PieceType, type),
-	//                __traits(getMember, PieceColor, color),
-	//                __traits(getMember, ArmyType, army));
-	//
-	//            auto e = p.to!PieceEnum;
-	//
-	//            writef("%08b %s", *cast(byte*)&p, e);
-	//
-	//            if  ((e & PieceEnum.PieceMask) == PieceEnum.PieceMask ||
-	//                 (e & PieceEnum.ColoredPieceMask) == PieceEnum.ColoredPieceMask ||
-	//                 (e & PieceEnum.ArmyTypeMask) == PieceEnum.ArmyTypeMask)
-	//            {
-	//                writeln();
-	//                continue;
-	//            }
-	//
-	//            writeln("\tASCII Representation: ", to!char(e));
-	//        }
+	Board b = new Board("r......." ~
+						"........" ~
+						".p..r..." ~
+						"........" ~
+						"....R..." ~
+						"....P..." ~
+						"........" ~
+						"........");
+	Piece rook = b[2,4];
+	rook.updatePositionsToHit();
+	assert(rook.getPositionsToHit().length == 9);
+	assert(rook.canHit(BoardPosition(4,4))); // enemy piece
+	assert(!rook.canHit(BoardPosition(2,1))); // friendly piece
+
+	Piece rook2 = b[4,4];
+	rook2.updatePositionsToHit();
+	assert(rook2.getPositionsToHit().length == 9);
+
+	Piece rook3 = b[0,0];
+	rook3.updatePositionsToHit();
+	assert(rook3.getPositionsToHit().length == 14);
 }
 
-//BitField:
-// 7|6 5 4|3|2 1 0
-// R ARMTY C PIECE
-// R - Reserved, ARMTY - ArmyType from Chess2 (0 for standard chess)
-// C - Color (0 for white, 1 for black),
-// PIECE - piece type (empty, pawn, knight, bishop, rook, Queen, king)
-//enum PieceEnum : byte
-//{
-//    Empty = 0,
-//    Pawn = 1,
-//    Knight = 2,
-//    Bishop = 3,
-//    Rook = 4,
-//    Queen = 5,
-//    King = 6,
-//
-//    PieceMask = 7,
-//
-//    //Color
-//    White = 0,
-//    Black = 1 << 3,
-//
-//    ColorMask = Black,
-//
-//    //Colored pieces
-//    White_Pawn = 1,
-//    White_Knight = 2,
-//    White_Bishop = 3,
-//    White_Rook = 4,
-//    White_Queen = 5,
-//    White_King = 6,
-//
-//    Black_Pawn =    Black + Pawn,
-//    Black_Knight =  Black + Knight,
-//    Black_Bishop =  Black + Bishop,
-//    Black_Rook =    Black + Rook,
-//    Black_Queen =   Black + Queen,
-//    Black_King =    Black + King,
-//
-//    ColoredPieceMask = 15,
-//
-//    //Army types
-//    Classic = 0 << 4,
-//    Nemesis = 1 << 4,
-//    Empowered = 2 << 4,
-//    Reaper = 3 << 4,
-//    TwoKings = 4 << 4,
-//    Animals = 5 << 4,
-//
-//    ArmyTypeMask = cast(byte)((1 << 6) + (1 << 5)+ (1 << 4))
-//}
+class Bishop : Piece
+{
+	this(PieceColor color, BoardPosition initialPosition, Board parentBoard)
+	{
+		super(color, initialPosition, parentBoard);
+	}
 
-import std.stdio;
+	override PieceEnum getType()
+	{
+		return PieceEnum.Bishop;
+	}
+
+	override BoardPosition[] getPositionsToHitImpl()
+	{
+		BoardPosition[] result;
+		result.reserve(20);
+		result ~= parentBoard.getUpLeftDiagonal(color, position);
+		result ~= parentBoard.getDownLeftDiagonal(color, position);
+		result ~= parentBoard.getUpRightDiagonal(color, position);
+		result ~= parentBoard.getDownRightDiagonal(color, position);
+		return result;
+	}
+}
+unittest
+{
+	Board b = new Board("b......." ~
+						"........" ~
+						"....r..." ~
+						"....B..." ~
+						"....R..." ~
+						"....P..." ~
+						"........" ~
+						"........");
+	Piece bishop = b[0,0];
+	bishop.updatePositionsToHit();
+	assert(bishop.getPositionsToHit().length == 4);
+	assert(bishop.canHit(BoardPosition(4,4)));
+	assert(bishop.canHit(BoardPosition(1,1)));
+	bishop = b[3,4];
+	bishop.updatePositionsToHit();
+	assert(bishop.getPositionsToHit().length == 13);
+}
+class Queen : Piece
+{
+	this(PieceColor color, BoardPosition initialPosition, Board parentBoard)
+	{
+		super(color, initialPosition, parentBoard);
+	}
+
+	override PieceEnum getType()
+	{
+		return PieceEnum.Queen;
+	}
+
+	override BoardPosition[] getPositionsToHitImpl()
+	{
+		BoardPosition[] result;
+		result.reserve(20);
+		result ~= parentBoard.getDownVertical(color, position);
+		result ~= parentBoard.getUpVertical(color, position);
+		result ~= parentBoard.getRightHorizontal(color, position);
+		result ~= parentBoard.getLeftHorizontal(color, position);
+		result ~= parentBoard.getUpLeftDiagonal(color, position);
+		result ~= parentBoard.getDownLeftDiagonal(color, position);
+		result ~= parentBoard.getUpRightDiagonal(color, position);
+		result ~= parentBoard.getDownRightDiagonal(color, position);
+		return result;
+	}
+}
+
+class Knight : Piece
+{
+	this(PieceColor color, BoardPosition initialPosition, Board parentBoard)
+	{
+		super(color, initialPosition, parentBoard);
+	}
+
+	override PieceEnum getType()
+	{
+		return PieceEnum.Knight;
+	}
+
+	override BoardPosition[] getPositionsToHitImpl()
+	{
+		BoardPosition[] result;
+		result.reserve(8);
+		parentBoard.appendPosition(result, BoardPosition(position.x + 2, position.y + 1), color);
+		parentBoard.appendPosition(result, BoardPosition(position.x + 2, position.y - 1), color);
+		parentBoard.appendPosition(result, BoardPosition(position.x + 1, position.y + 2), color);
+		parentBoard.appendPosition(result, BoardPosition(position.x + 1, position.y - 2), color);
+		parentBoard.appendPosition(result, BoardPosition(position.x - 2, position.y + 1), color);
+		parentBoard.appendPosition(result, BoardPosition(position.x - 2, position.y - 1), color);
+		parentBoard.appendPosition(result, BoardPosition(position.x - 1, position.y + 2), color);
+		parentBoard.appendPosition(result, BoardPosition(position.x - 1, position.y - 2), color);
+		return result;
+	}
+}
+unittest
+{
+	Board b = new Board("n......." ~
+						"........" ~
+						"....r..." ~
+						"........" ~
+						"...N...." ~
+						".....P.." ~
+						"........" ~
+						"........");
+	Piece n = b[0,0];
+	n.updatePositionsToHit();
+	assert(n.getPositionsToHit().length == 2);
+	assert(n.canHit(BoardPosition(2,1)));
+	assert(n.canHit(BoardPosition(1,2)));
+	n = b[4, 3];
+	n.updatePositionsToHit();
+	assert(n.getPositionsToHit().length == 7);
+	assert(n.canHit(BoardPosition(2,4))); //enemy piece
+	assert(!n.canHit(BoardPosition(5,5))); //friendly piece
+}
+
+class King : Piece
+{
+	this(PieceColor color, BoardPosition initialPosition, Board parentBoard)
+	{
+		super(color, initialPosition, parentBoard);
+	}
+
+	override PieceEnum getType()
+	{
+		return PieceEnum.King;
+	}
+
+	override BoardPosition[] getPositionsToHitImpl()
+	{
+		BoardPosition[] result;
+		parentBoard.appendPosition(result, BoardPosition(position.x + 1, position.y + 1), color);
+		parentBoard.appendPosition(result, BoardPosition(position.x + 1, position.y - 1), color);
+		parentBoard.appendPosition(result, BoardPosition(position.x + 1, position.y), color);
+		parentBoard.appendPosition(result, BoardPosition(position.x, position.y - 1), color);
+		parentBoard.appendPosition(result, BoardPosition(position.x, position.y + 1), color);
+		parentBoard.appendPosition(result, BoardPosition(position.x - 1, position.y - 1), color);
+		parentBoard.appendPosition(result, BoardPosition(position.x - 1, position.y + 1), color);
+		parentBoard.appendPosition(result, BoardPosition(position.x - 1, position.y), color);
+		return result;
+	}
+}
 
