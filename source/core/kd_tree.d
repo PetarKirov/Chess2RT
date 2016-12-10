b8e9ae37aecb62ae1dfa2fa29e3732ecfc424212
ZombineDev <petar.p.kirov@gmail.com>
2016-12-10 23:46:39 +0200

    Use uint instead of int in core.kd_tree for indexing triangles

--- source/core/kd_tree.d
+++ source/core/kd_tree.d
@@ -9,26 +9,26 @@ struct KDTreeNode
     double splitPos;
     union
     {
-        const(int[])*   triangles; // 1 pointer to list of triangle indices
-        KDTreeNode[2]*  children;  // 1 pointer to TWO children (children[0] and children[1])
+        const(uint[])* triangles; // 1 pointer to list of triangle indices
+        KDTreeNode[2]* children;  // 1 pointer to TWO children (children[0] and children[1])
     }
 
     /// Initialize this node as a leaf node.
-    void initLeaf(const int[] triangleList)
+    void initLeaf(const uint[] triangleList)
     {
-        struct Array { size_t len; const int* ptr; }
+        struct Array { size_t len; const uint* ptr; }
 
-        axis = Axis.None;
-        splitPos = 0;
-        triangles = cast(int[]*)new Array(triangleList.length, triangleList.ptr);
+        this.axis = Axis.None;
+        this.splitPos = 0;
+        this.triangles = cast(uint[]*)new Array(triangleList.length, triangleList.ptr);
     }
 
     /// Initialize this node as a in-node (a binary node with two children)
     void initBinary(Axis axis, double splitPos)
 	{
-        axis = axis;
-        splitPos = splitPos;
-        children = cast(KDTreeNode[2]*)(new KDTreeNode[2]).ptr;
+        this.axis = axis;
+        this.splitPos = splitPos;
+        this.children = cast(KDTreeNode[2]*)(new KDTreeNode[2]).ptr;
     }
 
     ~this()
