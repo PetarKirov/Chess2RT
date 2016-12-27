e4772e068ec40339b5906a3dadb6ce5c7aa19f52
ZombineDev <petar.p.kirov@gmail.com>
2016-12-27 22:07:27 +0200

    Improve convert_cubemaps.sh:
    + Ask to install imagemagick if needed
    + Go to ./data/env/ folder automatically
    + Download sample cubemaps automatically
    + Cleanup *.zip files too

--- scripts/convert_cubemaps.sh
+++ scripts/convert_cubemaps.sh
@@ -1,7 +1,21 @@
 # Convert skymaps from http://www.custommapmakers.org/skyboxes.php to
 # known format.
 
-# Assumes there are a couple of *.zip files each containing cubemap images.
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' imagemagick | grep "install ok installed")
+if [ "" == "$PKG_OK" ]; then
+   echo -e "Command not found! Install? (y/n) \c"
+   read
+   if [ "$REPLY" == "y" ]; then
+      sudo apt-get install imagemagick
+   fi
+fi
+
+set -ex
+
+cd ./data/env/
+
+wget www.custommapmakers.org/skyboxes/zips/hw_morning.zip
+wget www.custommapmakers.org/skyboxes/zips/hw_glacier.zip
 
 find -type f -iname '*.zip' | while read f; do unzip -j "$f" -d "${f%.*}"; done
 find -type f -iname '*.tga' -not -iname "*lf*" | while read f; do convert "$f" "${f%.*}.bmp"; done
@@ -16,7 +30,8 @@ find -type f -iname '*lf*.bmp' | while read f; do mv -v "$f" "${f%/*}/negz.bmp";
 find -type f -iname '*bk*.bmp' | while read f; do mv -v "$f" "${f%/*}/negx.bmp"; done
 find -type f -iname '*dn*.bmp' | while read f; do mv -v "$f" "${f%/*}/negy.bmp"; done
 
+# Cleanup
 find -type f \
-   \( -iname 'readme*' -or -iname '*.txt' -or \
+   \( -iname '*.zip' -or -iname 'readme*' -or -iname '*.txt' -or \
       -iname '*.shader' -or -iname '*.tga' -or -iname '*.jpg' \) \
    -exec rm -v {} \;
